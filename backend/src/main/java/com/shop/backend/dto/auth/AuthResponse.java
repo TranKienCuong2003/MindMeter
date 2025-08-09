0b6a22ca0516de450c8c7384939ab3b18ffcbebc
Tran Kien Cuong <trankiencuong30072003@gmail.com>
2025-08-09 10:18:59 +0700

    feat: add AuthResponse DTO in backend

---
+++ backend/src/main/java/com/shop/backend/dto/auth/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.shop.backend.dto.auth;
+
+import com.shop.backend.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private Role role;
+} 
